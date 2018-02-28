9393578bef25f00425920ef697418cc90c3e44a0
xiong <1099446868@qq.com>
2018-02-28 06:48:10 +0000

    new a work

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>@yield('title','Sample') -- Laravel 新手入门</title>
+    <title>@yield('title','Sample')新手入门</title>
     <!--yield（）也可以接受两个参数，第一个是占位参数，第二个是默认值，表示当指定变量的值为空时使用默认值.
     也可以在 yield 区块后面进行内容拼接。让我们标题拥有更加丰富的信息。-->
 </head>
